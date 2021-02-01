9a5010aefc9df916967cc05b439f8710ab781093
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-02-01 12:35:05 -0500

    dealing with conditional operators

--- guessinggame.py
+++ guessinggame.py
@@ -4,19 +4,34 @@ print("Please guess a number between 1 and 10: ")
 # Need to convert the input to an integer because it is default a str
 guess = int(input())
 
-if guess < answer:
-    print("Please guess a higher number")
-    guess = int(input())
-    if guess == answer:
-        print("Well done, you guessed it")
+if guess != answer:
+    if guess < answer:
+        print("Please guess higher")
     else:
-        print("Sorry you did not guess correctly")
-elif guess > answer:
-    print("Please guess a lower number")
-    guess = int(input())
+        print("Please guess lower")
+    quess = int(input())
     if guess == answer:
-        print("Well done, you guessed it")
+        print("well done, you guessed it")
     else:
-        print("Sorry you did not guess correctly")
+        print("sorry, you have not guessed correctly")
 else:
-    print("You guessed the number right!")
+    print("You guessed right the first time!")
+
+
+# if guess < answer:
+#     print("Please guess a higher number")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry you did not guess correctly")
+# elif guess > answer:
+#     print("Please guess a lower number")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry you did not guess correctly")
+# else:
+#     print("You guessed the number right!")
+
