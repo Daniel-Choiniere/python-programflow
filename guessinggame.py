61621c252076bf7b76b567fa0b98f89094534636
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-02-01 12:05:51 -0500

    adding a second guess to the game

--- guessinggame.py
+++ guessinggame.py
@@ -1,4 +1,4 @@
-answer= 5
+answer = 5
 
 print("Please guess a number between 1 and 10: ")
 # Need to convert the input to an integer because it is default a str
@@ -6,7 +6,17 @@ guess = int(input())
 
 if guess < answer:
     print("Please guess a higher number")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("Sorry you did not guess correctly")
 elif guess > answer:
     print("Please guess a lower number")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("Sorry you did not guess correctly")
 else:
     print("You guessed the number right!")
