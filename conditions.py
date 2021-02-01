a0a324c649fe4814fbd4cd86c7b1b8e493c8f824
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-02-01 13:24:54 -0500

    simplify chanined conditionals

--- conditions.py
+++ conditions.py
@@ -1,5 +1,14 @@
 age = int(input("How old are you? "))
 
-if age >= 16 and age <= 65:
+# if age >= 16 and age <= 65: is the same as the simplified chained conditional below
+if 16 <= age <= 65:
     print("Have a good day at work")
+else:
+    print("Enjoy your free time")
 
+print("-" * 80)
+
+if age < 16 or age > 65:
+    print("Enjoy your free time")
+else:
+    print("have a good day at work")
