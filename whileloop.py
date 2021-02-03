a33be7744560215b4620af1858fcfccb4260ba22
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-02-02 21:54:35 -0500

    simple direction guessing game using while loop

--- whileloop.py
+++ whileloop.py
@@ -2,8 +2,16 @@
 #     print("i is now {}".format(i))
 
 # same thing using while loop
-
 i = 0;
-while i < 10:
-    print("i is now {}".format(i))
-    i += 1
+# while i < 10:
+#     print("i is now {}".format(i))
+#     i += 1
+
+avaiable_exits = ["north", "south", "east", "west"]
+
+chosen_exit = ""
+while chosen_exit not in avaiable_exits:
+    chosen_exit = input("Please choose another direction: ")
+
+print("Aren't you glad you got out of there!")
+
