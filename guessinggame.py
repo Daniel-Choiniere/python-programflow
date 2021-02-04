c9763519aac053b0575225c41385fd3b6e6977b6
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-02-03 21:05:32 -0500

    used the random module to generate a random number for answer. also included custom response for ending game

--- guessinggame.py
+++ guessinggame.py
@@ -1,51 +1,31 @@
-answer = 5
+import random
 
-print("Please guess a number between 1 and 10: ")
+highest_value = 10
+answer = random.randint(1, highest_value)
+print(answer) # only for testing purposes - REMOVE AFTER TESTING
+
+print("Please guess a number between 1 and {}: ".format(highest_value))
 # Need to convert the input to an integer because it is default a str
 guess = int(input())
 
 if guess == answer:
     print("You guessed right the first time!")
+
 else:
-    if guess < answer:
-        print("Please guess higher")
-    else:
-        print("Please guess lower")
-    quess = int(input())
-    if guess == answer:
-        print("well done, you guessed it")
-    else:
-        print("sorry, you have not guessed correctly")
+    while guess != answer and guess != 0:
+        if guess < answer:
+            print("Please guess higher, or enter 0 to exit game")
+        else:
+            print("Please guess lower, or enter 0 to exit game")
+        guess = int(input())
+        if guess == answer:
+            print("well done, you guessed it")
+        else:
+            if guess == 0:
+                print("Exit game. Thanks for playing, try again sometime!")
+            else:
+                print("sorry, you have not guessed correctly")
 
 
-# if guess != answer:
-#     if guess < answer:
-#         print("Please guess higher")
-#     else:
-#         print("Please guess lower")
-#     quess = int(input())
-#     if guess == answer:
-#         print("well done, you guessed it")
-#     else:
-#         print("sorry, you have not guessed correctly")
-# else:
-#     print("You guessed right the first time!")
 
-
-# if guess < answer:
-#     print("Please guess a higher number")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("Sorry you did not guess correctly")
-# elif guess > answer:
-#     print("Please guess a lower number")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("Sorry you did not guess correctly")
-# else:
-#     print("You guessed the number right!")
 
